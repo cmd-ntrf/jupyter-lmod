0235198e78025b6f1481a0fce5a754652bd5c2c7
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2016-02-23 17:59:31 -0500

    Remove LmodHandler keeping only LmodActionHandler

--- notebook_lmod_ext/__init__.py
+++ notebook_lmod_ext/__init__.py
@@ -7,27 +7,6 @@ import lmod
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
 
-from jinja2 import Template
-
-modules_template = """
-<html><body><form method="POST">
-{% for loc, modules in moduleavail.items() %}
-  <h2>{{ loc }}</h2>
-  <table>
-      <tr>
-      {% for module in modules %}
-      <td><input type="checkbox" name="module" value="{{ module }}" {% if module in modulelist %} checked {% endif %}> {{ module }} </td>
-      {% if loop.index % 4 == 0 %}
-            </tr><tr>
-      {% endif %}
-      {% endfor %}
-      </tr>
-  </table>
-{% endfor %}
-<input type="submit">
-</form></body></html>
-"""
-
 class LmodActionHandler(IPythonHandler):
     @tornado.web.authenticated
     def get(self, action):
@@ -44,40 +23,12 @@ class LmodActionHandler(IPythonHandler):
                 lmod.module(action, module)
                 self.finish(json.dumps('SUCCESS'))
 
-class LmodHandler(IPythonHandler):
-    template = Template(modules_template)
-
-    def initialize(self):
-        self.module_list = module_list()
-
-    @tornado.web.authenticated
-    def get(self):
-        self.write(self.template.render(moduleavail=module_avail(),
-                                        modulelist=self.module_list))
-
-    @tornado.web.authenticated
-    def post(self):
-        checked_modules = self.request.arguments['module']
-        new_modulelist = set(map(bytes.decode, checked_modules))
-
-        to_load = new_modulelist - self.module_list
-        to_unload = self.module_list - new_modulelist
-
-        lmod.module('load', " ".join(to_load))
-        lmod.module('unload', " ".join(to_unload))
-
-        self.module_list = set(lmod.module_list())
-        self.write(self.template.render(moduleavail=module_avail(),
-                                        modulelist=self.module_list))
-
-
 #-----------------------------------------------------------------------------
 # URL to handler mappings
 #-----------------------------------------------------------------------------
 _lmod_action_regex = r"(?P<action>load|unload|avail|list)"
 
 default_handlers = [
-    (r"/lmod", LmodHandler),
     (r"/lmod/%s" % (_lmod_action_regex), LmodActionHandler),
 ]
 
@@ -99,6 +50,6 @@ def load_jupyter_server_extension(nbapp):
     nbapp.config_manager.update('tree', {"load_extensions": {"lmod": True}})
 
 if __name__ == "__main__":
-    app = tornado.web.Application([ (r"/", LmodHandler), ])
+    app = tornado.web.Application([ (r"/", LmodActionHandler), ])
     app.listen(12345)
     tornado.ioloop.IOLoop.current().start()
