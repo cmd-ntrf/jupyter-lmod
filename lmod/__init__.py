845b38771757f1480d30a85a04310cd4cf8fd21a
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2016-02-24 17:52:52 -0500

    Extract modules group naming from module_avail function

--- lmod/__init__.py
+++ lmod/__init__.py
@@ -17,17 +17,29 @@ def module(command, arguments=""):
 
     return result.stderr.read().decode()
 
-def module_avail():
+def colosse_filter(entry):
+    if entry.startswith('/software6/modulefiles'):
+        entry = os.path.relpath(entry, '/software6/modulefiles')
+    elif entry.startswith('/rap'):
+        entry = 'Your groups\' modules'
+    elif entry.startswith('/home'):
+        entry = 'Your personal modules'
+    if entry.startswith('Compilers'):
+        entry = 'Compiler-dependent modules'
+    if entry.startswith('Core'):
+        entry = 'Core Modules'
+    return entry
+
+def module_avail(filter_=colosse_filter):
     string = module('avail')
     list_ = string.split()
     locations = OrderedDict()
     cur_location = None
     for i, entry in enumerate(list_):
         if entry.startswith('/'):
-            if entry.startswith('/software6/modulefiles'):
-                entry = os.path.relpath(entry, '/software6/modulefiles')
-            cur_location = entry
-            locations[cur_location] = []
+            cur_location = filter_(entry)
+            if not cur_location in locations:
+                locations[cur_location] = []
         elif (re.match("[A-Za-z\/]*", entry) and
               i != (len(list_) - 1) and
               list_[i+1].startswith(entry)):
