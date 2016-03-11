8233cfbf27c806cac63afccb9a8d3d2bf753e6e8
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2016-03-11 14:51:37 -0500

    Small refactoring of kernelspec manager

--- kernel_spec_lmod_manager/__init__.py
+++ kernel_spec_lmod_manager/__init__.py
@@ -3,10 +3,8 @@
 from jupyter_client.kernelspec import KernelSpec, KernelSpecManager
 
 import json
-import io
-import os
 import lmod
-pjoin = os.path.join
+from os.path import join as pjoin
 
 class KernelSpecLMod(KernelSpec):
     @classmethod
@@ -14,11 +12,10 @@ class KernelSpecLMod(KernelSpec):
         """
         """
         kernel_file = pjoin(resource_dir, 'kernel.json')
-        with io.open(kernel_file, 'r', encoding='utf-8') as f:
+        with open(kernel_file, 'r', encoding='utf-8') as f:
             kernel_dict = json.load(f)
-        if 'lmod' in kernel_dict:
-            for module in kernel_dict['lmod']:
-                lmod.module('load', module)
+        for module in kernel_dict.get('lmod', []):
+	    lmod.module('load', module)
         return cls(resource_dir=resource_dir, **kernel_dict)
 
 class KernelSpecLModManager(KernelSpecManager):
