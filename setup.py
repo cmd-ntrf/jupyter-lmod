ae5d5f9070db018fac69b9843913e9e9736bb494
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2017-04-02 23:30:43 -0400

    Simplify setup.py import
    The version checking is handled by jupyter which jupyterlmod depends on.

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Copyright (c) Juptyer Development Team.
-# Distributed under the terms of the Modified BSD License.
-
-#-----------------------------------------------------------------------------
-# Minimal Python version sanity check (from IPython/Jupyterhub)
-#-----------------------------------------------------------------------------
-from __future__ import print_function
-
-import os
-import sys
-
-v = sys.version_info
-if v[:2] < (3,3):
-    error = "ERROR: Jupyter Hub requires Python version 3.3 or above."
-    print(error, file=sys.stderr)
-    sys.exit(1)
-
-
-if os.name in ('nt', 'dos'):
-    error = "ERROR: Windows is not supported"
-    print(error, file=sys.stderr)
-
-# At least we're on the python version we need, move on.
 from setuptools import setup
 
 setup_args = dict(
